8ccb7aaed2d0085a65750b1116d9fbc76d9bd945
Wayne Dahlberg <waynefj40@gmail.com>
2024-06-23 20:42:26 -0600

    Initial Commit

---
+++ TallyHo/TallyHoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TallyHoApp.swift
+//  TallyHo
+//
+//  Created by Wayne Dahlberg on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TallyHoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
